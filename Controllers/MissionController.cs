ebfbc6a8a459ed7f6df8dd09ec029186150846a2
Curtis Godzisz <curtisgodzisz@gmail.com>
2020-08-26 09:12:45 -0400

    Working with Databases

---
+++ Controllers/MissionController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Supers.Models;
+
+namespace Supers.Controllers
+{
+    public class MissionController : Controller
+    {
+        private readonly SupersDbContext _context;
+
+        public MissionController(SupersDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Mission()
+        {
+            List<Mission> mission = _context.Mission.ToList();
+            return View(mission);
+        }
+
+        [HttpGet]//this method is simply displaying in fo in the view
+        public IActionResult AddMission()
+        {
+            return View();
+        }
+
+        [HttpPost]//this method is recieveing information from the view(in this case, a Super object)
+        public IActionResult AddMission(Mission newMission)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Mission.Add(newMission);
+                _context.SaveChanges();//dont forget this  saves it back to sql
+            }
+
+            return RedirectToAction("Mission");//RedirectTo Action goes to method index
+        }
+
+        public IActionResult DeleteMission(int id)
+        {
+            //find the super we want to delete from the database
+            //Find() will bring back an entire Super via its primary key
+            var foundMission = _context.Mission.Find(id);
+            if (foundMission != null)
+            {
+                _context.Mission.Remove(foundMission);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Mission");
+        }
+    }
+}
---
+++ Controllers/SupersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Supers.Models;
+
+namespace Supers.Controllers
+{
+    public class SupersController : Controller
+    {
+        private readonly SupersDbContext _supersdb;
+       
+        public SupersController(SupersDbContext context)
+        {
+            _supersdb = context;
+        }
+       
+        public IActionResult Supers()
+        {
+            List<Super> super = _supersdb.Super.ToList();
+            return View(super);
+        }
+
+        [HttpGet]//this method is simply displaying in fo in the view
+        public IActionResult AddSuper()
+        {
+            return View();
+        }
+
+        [HttpPost]//this method is recieveing information from the view(in this case, a Super object)
+        public IActionResult AddSuper(Super newSuper)
+        {
+            if (ModelState.IsValid)
+            {
+                _supersdb.Super.Add(newSuper);
+                _supersdb.SaveChanges();//dont forget this  saves it back to sql
+            }
+
+            return RedirectToAction("Index");//RedirectTo Action goes to method index
+        }
+
+        public IActionResult DeleteSuper(int id)
+        {
+            //find the super we want to delete from the database
+            //Find() will bring back an entire Super via its primary key
+            var foundSuper = _supersdb.Super.Find(id);
+            if(foundSuper != null)
+            {
+                _supersdb.Super.Remove(foundSuper);
+                _supersdb.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
